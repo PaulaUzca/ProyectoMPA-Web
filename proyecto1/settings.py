7dea4e4bd544777c5bf9e77f8d0da8e4307662b8
JuanSepu18 <juanestebans901@gmail.com>
2022-09-12 14:07:44 -0500

    commit

--- proyecto1/settings.py
+++ proyecto1/settings.py
@@ -69,12 +69,12 @@ WSGI_APPLICATION = 'proyecto1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("DB_NAME"),
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USERNAME"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
-        'PORT': 5432,
+      'PORT': 5432,
     }
 }
 
